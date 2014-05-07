4ffb14e219c4e3c09ad4e4b009f020acb87c5b29
githubxander <XanderRoyce@molavenet.com>
2014-05-07 23:03:47 +0800

    startup admin lte

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 //app.initialize();
 
 
-var hostname = 'http://saynsnap.com/';
-var domainApi = 'http://saynsnap.com/mobileapi/api/';
-var domainSite = 'http://saynsnap.com/application/';
-var domainMobile = 'http://saynsnap.com/mobile/';    
+var hostname = 'http://localhost/mobile/biofem/';
+var domainApi = 'http://localhost/mobile/biofem/';
+var domainSite = 'http://localhost/mobile/biofem/';
+var domainMobile = 'http://localhost/mobile/biofem/';    
 
 
 function init()
@@ -13,14 +13,6 @@ function init()
 	delete init;
 }
 
-function checkConnection() {
-    
-    if(!navigator.onLine){
-        alert('Internet connection problem.');
-        window.location = 'index.html';
-    }
-    return false;
-}
 
 function loadMenu()
 {
@@ -46,14 +38,14 @@ function loadMenu()
 		return false;				
 	});
 	
-	$('.logout').click(function(){
-                   
-		localStorage.clear();
-		var url = 'index.html';
-
-		window.location = url;
-		return false;
-	});
+	//$('.logout').click(function(){
+//                   
+//		localStorage.clear();
+//		var url = 'index.html';
+//
+//		window.location = url;
+//		return false;
+//	});
 }
 
 	
@@ -66,58 +58,6 @@ function checkPreAuth() {
 //        handleLogin();
 //    }
 }
-
-
-function handleLogin() {
-    
-    var form = $("#frmLogin");  
-    
-    //disable the button so we can't resubmit while we wait
-    $("#submitLogin", form).attr("disabled","disabled");
-    
-	var e = $("#username", form).val();
-	var p = $("#password", form).val();
-    
-    if(e != '' && p!= '') {
-        $("#submitLogin",form).attr("disabled","disabled");
-		$.post (domainApi + 'login', {
-			username	: e,
-			password	: p,
-		}, function(data){
-                
-			if (data.status == 'success') {
-                window.localStorage["username"] 	= e;
-                window.localStorage["password"] = p;
-                window.localStorage["user_id"] 	= data.user_id;
-				
-				if (window.localStorage["last_visit"] == '' || typeof window.localStorage['last_visit'] === "undefined") {
-					var path = 'home.html';
-				} else {
-					var path = window.localStorage["last_visit"];
-				}
-
-				var home = domainMobile + path;
-				
-				setTimeout(function() {                           
-                    path == 'home.html'
-					$.mobile.changePage(path, {
-						transition	: "slide",
-					});
-				}, 2000);
-
-			} else {
-				alert('Login Failed: Incorrect username or password');
-                ajaxLoader(0);
-                $("#submitLogin").removeAttr("disabled");
-                navigator.notification.alert("Your login failed", function() {});
-            }
-		});
-        
-    } else {
-        $("#submitLogin").removeAttr("disabled");
-    }
-}
-
 
 
 
@@ -134,34 +74,35 @@ function deviceReady()
 	*/
 }
 
-
 $(document).bind( "mobileinit", function() {
 	// Make your jQuery Mobile framework configuration changes here!
 	$.mobile.allowCrossDomainPages = true;
 });
 
-$(document).on('pageinit','#login-page', function(){
-         
-     
-    $( "#frmLogin" ).validate({
-        submitHandler: function( form ) {
-            ajaxLoader(1);
-            checkConnection();
-            handleLogin();
-            
-        }
-    });
-    
-	checkPreAuth();
-
-});	
 
 //Init Homepage
-$(document).on('pageinit','#home-page', function(){
+$(document).on('pageinit','#dashboard-page', function(){
+    
     
 });
+
+//Init Homepage
+$(document).on('pageinit','#data-page', function(){
+    
+        $(function() {
+            $("#example1").dataTable();
+        });
+  
+            
+});
+
+
+
+
+
 
 
 $(document).on('pageinit','[data-role=page]', function(){
 	loadMenu();
 });
+
